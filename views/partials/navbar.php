568ec3f3e24294214d90b5ceaf299184ab268a91
PirateKenny01 <shadab.fardin0234@gmail.com>
2026-05-16 23:34:20 +0600

    add task 2 admin dashboard navbar-Dashboard,category,Brands,Products

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -6,6 +6,12 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'guest';
         <a href="home.php">Home</a> |
         <a href="profile.php">Profile</a> |
         <a href="admin_create_user.php">Admin Create User</a> |
+
+        <a href="Admin_Dashboard.php">Dashboard</a> | 
+        <a href="categories.php">Categories</a> | 
+        <a href="brands.php">Brands</a> | 
+        <a href="products.php">Products</a> |
+        
         <a href="../controllers/logout.php">Logout</a>
     <?php } else if ($role == 'customer') { ?>
         <a href="home.php">Home</a> |
