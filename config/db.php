3f7db309efec32fac403e5616aed503a836a0dd7
PirateKenny01 <shadab.fardin0234@gmail.com>
2026-05-16 23:36:32 +0600

    modified,add error handling

--- config/db.php
+++ config/db.php
@@ -1,15 +1,23 @@
 <?php
 
-$host = "127.0.0.1";
-$dbuser = "root";
-$dbpassword = "";
-$dbname = "ocs";
+    $host = "127.0.0.1";
+    $dbuser = "root";
+    $dbpassword = "";
+    $dbname = "ocs";
 
-function getConnection()
+
+    function getConnection()
 {
-    global $host;
-    global $dbuser;
-    $con = mysqli_connect($host, $dbuser, $GLOBALS['dbpassword'], $GLOBALS['dbname']);
-    return $con;
-}
+        global $host;
+        global $dbuser;
+        $con = @mysqli_connect($host, $dbuser, $GLOBALS['dbpassword'], $GLOBALS['dbname']);
+        
+        //if (!$con) 
+        //{
+           // die("Database Connection Error: " . mysqli_connect_error() . "<br>Make sure MySQL is running in XAMPP.");
+        //}
+        
+        return $con; 
+    }
+
 ?>
